b3aed4d36bc600fcc098ee83817fe82fcbc2dc79
Ali Oğuz Can <alioguz.can@tum.de>
2024-06-27 14:44:20 +0200

    ADDED: confusion matrix

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import f1_score, accuracy_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 class Sklearn_Classifier():
     def __init__(self, sklearn_classifier) -> None:
@@ -7,7 +8,9 @@ class Sklearn_Classifier():
     def train_test(self, X_train, y_train, X_test, y_test):
         self.model = self.model.fit(X=X_train, y=y_train)
         y_pred = self.model.predict(X_test)
-        f1_s = f1_score(y_true=y_test, y_pred=y_pred, average="micro")
-        print(f"F1-score: {f1_s}")
+        f1_s = f1_score(y_true=y_test, y_pred=y_pred, average="macro")
+        print(f"F1-score macro: {f1_s}")
         accuracy = accuracy_score(y_true=y_test, y_pred=y_pred, normalize=False)/y_test.shape[0]
         print(f"Accuracy: {accuracy}")
+        disp = ConfusionMatrixDisplay(confusion_matrix(y_true=y_test, y_pred=y_pred))
+        disp.plot()
